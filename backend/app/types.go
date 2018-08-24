87726dd54162056757a5d2745d8f739b0f56058d
Ben.Woodhouse16 <ben.woodhouse@qa.com>
2018-08-24 12:48:22 +0100

    added basic go backend

---
+++ backend/app/types.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+//Todo list
+type Todo struct {
+	Name      string    `json:"name"`
+	Date      time.Time `json:"datedue"`
+	Completed bool      `"json:"completed"`
+}
+
+//list of Todo
+type Todos []Todo
+
+//end points
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+//collection of endpoints
+type Routes []Route
