05e6839c100325e17ea4b1ab21c8754c5c4e4d12
bwoodhouse322 <bwoodhouse322@gmail.com>
2018-09-07 13:50:21 +0100

    added deployment to minikube to cd pipeline

--- backend/cd-pipeline.sh
+++ backend/cd-pipeline.sh
@@ -3,12 +3,26 @@
 #
 ACCOUNTNAME=bwoodhouse322
 
+minikube status
+
+#If cluster isn't running, start cluster
+if [ $? -eq 1 ] ;
+then
+    minikube start  
+fi
+
+
 docker build -t todo-rest-api --file=docker/Dockerfile .
 
-IMAGEID=$(docker images | grep todo-rest-api | awk '{print $3}')
+IMAGEID=$(docker images | grep todo-rest-api | awk '{print $3}'| tail -n 1)
+
+echo $IMAGEID 
 
 docker tag $IMAGEID $ACCOUNTNAME/todo-rest-api
 
 docker push $ACCOUNTNAME/todo-rest-api 
 
+kubectl config use-context minikube
 
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
