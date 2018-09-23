4061652a74e8e15b3b97146aa07a37be5bd3829b
Ben.Woodhouse16 <bwoodhouse322@gmail.com>
2018-09-23 18:59:22 +0100

    fail safer

--- backend/cd-pipeline.sh
+++ backend/cd-pipeline.sh
@@ -6,7 +6,7 @@ APPNAME=todo-rest-api
 
 if [[../ci-functions/docker-build-push.sh -x &&  ../ci-functions/deploy-app.sh -x ]] ; then
 
-    ../ci-functions/docker-build-push.sh "${ACCOUNTNAME}" "${APPNAME}"
+    ../ci-functions/docker-build-push.sh "${ACCOUNTNAME}" "${APPNAME}" || exit 1
     ../ci-functions/deploy-app.sh
 else
     echo "Make sure ci-functions are all made executable" 
